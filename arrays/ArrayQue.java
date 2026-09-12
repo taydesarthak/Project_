37c19ad8f5711f4db25148a71b2609ce220f5491
taydesarthak <taydesarthak279@gmail.com>
2026-09-12 23:39:18 +0530

    array question add

---
+++ arrays/ArrayQue.java
@@ -0,0 +1,10 @@
+package arrays;
+
+public class ArrayQue {
+    public static void main(String[] args) {
+        int[] arr = {3,2,2,4,56,33,2};
+        for(int ele: arr){
+            System.out.print(ele + " ");
+        }
+    }
+}
